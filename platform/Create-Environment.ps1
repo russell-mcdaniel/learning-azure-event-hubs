70375ebac4c33019a142fc7fc9365791a095ccc8
Russell McDaniel <russell.mcdaniel@siliconesis.com>
2020-06-05 21:33:01 -0700

    Build out environment script and align configurations

--- platform/Create-Environment.ps1
+++ platform/Create-Environment.ps1
@@ -1,37 +1,82 @@
 # Requirements
 # * PowerShell 5.1 or newer.
 # * Azure PowerShell module - https://docs.microsoft.com/en-us/powershell/azure
+param (
+    [Parameter(Mandatory = $true)]
+    [guid]$SubscriptionId
+)
+
+# Environment Configuration
+$ResourceGroupName = "learning-azure-event-hubs"
+$ResourceLocation = "West US 2"
+
+$EventHubsNamespaceName = "learning-azure-event-hubs"
+$PartitionCount = 4
+$EventHubs = @(
+    "eventhub-01",
+    "eventhub-02",
+    "eventhub-03"
+)
+$ConsumerGroups = @(
+    "consumergroup-01",
+    "consumergroup-02",
+    "consumergroup-03"
+)
+
+$StorageAccountName = "learningeventhubs"
+$StorageContainerName = "checkpoints"
+
+$AppServicePlanName = "learning-azure-event-hubs"
+$WebApps = @(
+    "laeh-producer-01",
+    "laeh-producer-02",
+    "laeh-producer-03",
+
+    "laeh-consumer-01",
+    "laeh-consumer-02",
+    "laeh-consumer-03"
+)
 
 # Log in to Azure
-#Connect-AzAccount
-
-$resourceGroupName = "learning-azure-event-hubs"
-$resourceLocation = "West US 2"
-$eventHubsNamespaceName = "learning-azure-event-hubs"
-$storageAccountName = "learningeventhubs"
-$storageContainerName = "event-hubs-checkpoints"
+Connect-AzAccount -Subscription "$SubscriptionId"
 
 # Resource Group
-New-AzResourceGroup -ResourceGroupName $resourceGroupName –Location $resourceLocation
+New-AzResourceGroup -ResourceGroupName "$ResourceGroupName" -Location "$ResourceLocation"
 
-# Event Hub Namespace
-# Tier:                Basic
+# Event Hubs Namespace
+# Tier:                Standard
 # TU:                  1
-New-AzEventHubNamespace -ResourceGroupName $resourceGroupName -NamespaceName $eventHubsNamespaceName -Location $resourceLocation -SkuName Basic -SkuCapacity 1
+New-AzEventHubNamespace -ResourceGroupName "$ResourceGroupName" -NamespaceName "$EventHubsNamespaceName" -Location "$ResourceLocation" -SkuName "Standard" -SkuCapacity 1
 
-# Event Hub
+# Event Hubs / Event Hub Consumer Groups
 # Partitions:          4 (2 is default, minimum)
 # Retention:           1 (maximum for basic tier)
-New-AzEventHub -ResourceGroupName $resourceGroupName -NamespaceName $eventHubsNamespaceName -EventHubName hub-1 -PartitionCount 4 -MessageRetentionInDays 1
+Foreach ($EventHub in $EventHubs)
+{
+    New-AzEventHub -ResourceGroupName "$ResourceGroupName" -NamespaceName "$EventHubsNamespaceName" -EventHubName "$EventHub" -PartitionCount $PartitionCount -MessageRetentionInDays 1
+
+    Foreach ($ConsumerGroup in $ConsumerGroups)
+    {
+        New-AzEventHubConsumerGroup -ResourceGroupName "$ResourceGroupName" -NamespaceName "$EventHubsNamespaceName" -EventHubName "$EventHub" -ConsumerGroupName "$ConsumerGroup"
+    }
+}
 
 # Storage Account
 # SKU:                 Standard_LRS (includes performance tier and replication scope)
 # Kind:                StorageV2
 # Replication:         LRS
 # Tier:                Hot
-$storageAccount = New-AzStorageAccount -ResourceGroupName $resourceGroupName -StorageAccountName $storageAccountName -SkuName Standard_LRS -Location $resourceLocation -Kind StorageV2 -AccessTier Hot -EnableHttpsTrafficOnly $true
-$storageContext = $storageAccount.Context
+$storageAccount = New-AzStorageAccount -ResourceGroupName "$ResourceGroupName" -StorageAccountName "$StorageAccountName" -SkuName "Standard_LRS" -Location "$ResourceLocation" -Kind "StorageV2" -AccessTier "Hot" -EnableHttpsTrafficOnly $true
 
 # Blob Storage Container (Event Hub Processor Checkpointing)
 # Access:              Private
-New-AzStorageContainer -Container $storageContainerName -PublicAccess Off -Context $storageContext
+New-AzStorageContainer -Container "$StorageContainerName" -PublicAccess "Off" -Context $storageAccount.Context
+
+# App Service Plan
+New-AzAppServicePlan -ResourceGroupName "$ResourceGroupName" -Name "$AppServicePlanName" -Location "$ResourceLocation" -Tier "Free"
+
+# Web Apps
+Foreach ($WebApp in $WebApps)
+{
+    New-AzWebApp -ResourceGroupName "$ResourceGroupName" -Name "$WebApp" -Location "$ResourceLocation" -AppServicePlan "$AppServicePlanName"
+}
